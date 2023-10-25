c98fa94ef866ddca9cb955b65f3c7f923834c650
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-25 11:03:22 +0300

    added les2

---
+++ main.js
@@ -0,0 +1,12 @@
+5 > 2;
+5 < 2;
+5 <= 5;
+2 == "2"; // true
+2 === "2"; // false
+String(2) === "2"; // true
+2 != "2"; // false
+2 !== "2"; // true
+String(2) !== "2"; // false
+
+const a = 5;
+Boolean(a); //true
