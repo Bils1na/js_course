f3d42cb040726319f778c3dcefffc97d4a28b1a9
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-27 14:41:36 +0300

    added les3

--- lesson3/main.js
+++ lesson3/main.js
@@ -1,17 +1,137 @@
 "use strict";
 
-for (let i = 10, a = 50;
-     i >= 0 && a < 1000; 
-     i -= 2, a += 50) {
-         console.log(i);
-     }
+// for (let i = 10, a = 50;
+//      i >= 0 && a < 1000; 
+//      i -= 2, a += 50) {
+//          console.log(i);
+//      }
 
-while (true) {
-    const userInput = prompt("Enter number");
-    const userNumber = Number(userInput);
-    if (userNumber > 0 && Number.isInteger(userNumber)) {
-        console.log("Great");
-        break;
-    }
-    console.log("That's wrong!");
-}
+// while (true) {
+//     const userInput = prompt("Enter number");
+//     const userNumber = Number(userInput);
+//     if (userNumber > 0 && Number.isInteger(userNumber)) {
+//         console.log("Great");
+//         break;
+//     }
+//     console.log("That's wrong!");
+// }
+
+// const users = [
+//     { name: "Petr", age: 30 },
+//     { name: "Ivan", age: 25 },
+//     { name: "Fedor", age: 29 },
+//     { name: "Igor", age: 27 },
+//     { name: "Dean", age: 22 },
+// ];
+
+// let ageCount = 0;
+// for (let index = 0; index < users.length; index++) {
+//     console.log(users[i].name);
+//     ageCount += users[i].age;
+// }
+
+// console.log(ageCount);
+
+// const user = {
+//     id: 4,
+//     name: "John",
+//     socialNetworks: {
+//         vk: "https://vk.com/john",
+//         supersite: "https://supersite.com/john",
+//     },
+//     images: [
+//         "1.jpg",
+//         "2.svg",
+//         "3.gif",
+//     ],
+// };
+
+// console.log(user.social?.supersite);
+// console.log(user.images[1]);
+
+// if ("social" in user) {
+//     user.social.supersite;
+// }
+
+// for (let index = 0; index < user.images.length; index++) {
+//     console.log(user.images[i]);
+// }
+
+// for (const i in user.images) {
+//     console.log(user.images[i]);
+// }
+
+// for (const val of user.images) {
+//     console.log(val);
+// }
+
+// const images = [
+//             "1.jpg",
+//             "2.svg",
+//             "3.gif",
+//         ];
+
+// images.forEach(function (element, index) {
+//     console.log(`${index}. ${element}`);
+// })
+
+// const arr = [];
+// const arr2 = [];
+
+// arr === arr2; // false
+
+// const car1 = {
+//     model: "BMW",
+// };
+
+// const car2 = {
+//     model: "BMW",
+// };
+
+// console.log(car1 === car2); // false
+// console.log(car1 === car1); // true
+
+// const card1 = {
+//     deposit: 1_000,
+// };
+// card1.deposit = card1.deposit - 100;
+// console.log(card1);
+
+// const card2 = card1;
+// card2.deposit = card2.deposit - 100;
+
+// console.log(card1);
+
+// const obj = {
+//     a: 5,
+//     b: 10,
+// };
+
+// obj.c = obj;
+
+// console.log(Object.keys(obj));
+
+// for (const key in obj) {
+//         console.log(key, obj[key]);
+// }        
+
+// const newObj = Object.assign({}, obj);
+// const superNewObj = {...obj};
+
+
+const user = {
+    name: "Pavel",
+    surname: "Tarasov",
+};
+
+
+
+
+
+
+
+
+
+
+
+
