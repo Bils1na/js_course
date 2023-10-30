bc736465dd1b2422f96ed726ce96e4a119eae015
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-30 14:44:31 +0300

    added les4

---
+++ lesson4/main.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// const obj = {
+//     a: 1,
+//     __proto__: {
+//         b: 2,
+//         c: 3,
+//         __proto__: {
+//             z: 4,
+//             __proto__: null
+//         },
+//     },
+// };
+
+// console.log(obj.a); //1
+// console.log(obj.c); //3
+// console.log(obj.z); //4
+// console.log(obj.asd); //undefined
+
+// function toBeep() {
+//     console.log(`${this.model}: Beep!`); 
+// }
+
+// const myCar = {
+//     model: "BMW",
+//     // beep: function () {
+//     //   console.log("Beep!");  
+//     // },
+//     // beep() {
+//     //     console.log(`${this.model}: Beep!`); 
+//     //   },
+//     beep: toBeep,
+// };
+
+// const myCar2 = {
+//     model: "Toyota",
+//     // beep: function () {
+//     //   console.log("Beep!");  
+//     // },
+//     // beep() {
+//     //     console.log(`${this.model}: Beep!`);  
+//     //   },
+//     beep: toBeep,
+// };
+
+// myCar.beep();
+// myCar2.beep();
+
+function Car(model) {
+    // this = {};
+    // this.__proto__ = Car.prototype;
+    this.model = model;
+    // this.beep = function () {
+    //     console.log(`${this.model}: Beep!`);
+    // };
+
+    // return this;
+}
+
+// console.log(Car.prototype);
+Car.prototype.wheelsCount = 4;
+Car.prototype.beep = function () {
+        console.log(`${this.model}: Beep!`);
+    };
+
+const car1 = new Car("BMW");
+const car1 = new Car("Toyota");
+
+car1.beep();
+car2.beep();
