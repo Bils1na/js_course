fe9fc1f6b4e1bf6bbb50fe9c38b225f87e5ba864
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-30 16:03:11 +0300

    added les4

--- lesson4/main.js
+++ lesson4/main.js
@@ -46,29 +46,91 @@
 // myCar.beep();
 // myCar2.beep();
 
-function toBeep() {
-    console.log(`${this.model}: Beep!`);
+// function toBeep() {
+//     console.log(`${this.model}: Beep!`);
+// }
+
+// function Car(model) {
+//     // this = {};
+//     // this.__proto__ = Car.prototype;
+//     this.model = model;
+//     // this.beep = function () {
+//     //     console.log(`${this.model}: Beep!`);
+//     // };
+
+//     // return this;
+// }
+
+// // console.log(Car.prototype);
+// Car.prototype.wheelsCount = 4;
+// Car.prototype.beep = function () {
+//         console.log(`${this.model}: Beep!`);
+//     };
+
+// const car1 = new Car("BMW");
+// const car1 = new Car("Toyota");
+
+// car1.beep();
+// car2.beep();
+
+// function Person(name, sex) {
+//     this.name = name;
+//     this.sex = sex;
+// }
+
+// Person.prototype.callName = function () {
+//     console.log(`My name is ${this.name}`)
+// }
+
+// // const person1 = new Person("Pavel", "male");
+// // person1.callName();
+
+// function Employee(name, sex, position) {
+//     // this.name = name;
+//     // this.sex = sex;
+//     Person.call(this, name, sex);
+//     this.position = position;
+// }
+
+// // { sayPosition: f(), __proto__: Person.prototype }
+// Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype.constructor = Employee;
+
+// // Employee.prototype.callName = function () {
+// //     console.log(`My name is ${this.name}`)
+// // }
+
+// Employee.prototype.sayPosition = function () {
+//     console.log(`My position is ${this.position}`)
+// }
+
+// const employee1 = new Employee("Pavel", "male", "mover");
+// employee1.callName();
+// employee1.sayPosition();
+
+// []
+// Array.prototype.hello = function () {
+//     console.log("Hello");
+// };
+
+class Person {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    sayName() {
+        console.log(`My name is ${this.name}`);
+    }
 }
 
-function Car(model) {
-    // this = {};
-    // this.__proto__ = Car.prototype;
-    this.model = model;
-    // this.beep = function () {
-    //     console.log(`${this.model}: Beep!`);
-    // };
+class Employee extends Person {
+    constructor(name, sex, position) {
+        super(name, sex);
+        this.position = position;
+    }
 
-    // return this;
+    sayPosition() {
+        console.log(`My position is ${this.position}`);
+    }
 }
-
-// console.log(Car.prototype);
-Car.prototype.wheelsCount = 4;
-Car.prototype.beep = function () {
-        console.log(`${this.model}: Beep!`);
-    };
-
-const car1 = new Car("BMW");
-const car1 = new Car("Toyota");
-
-car1.beep();
-car2.beep();
