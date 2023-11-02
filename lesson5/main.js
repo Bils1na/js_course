5c912a3ec967cfbd91a6fc5ab7e497d8473eafbc
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-02 23:24:55 +0300

    styles in js

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,20 +1,26 @@
 "use strict";
 
-const btnEl = document.querySelector("button");
-btnEl.id;
-btnEl.id = "asd";
+// const btnEl = document.querySelector("button");
+// btnEl.id;
+// btnEl.id = "asd";
 
-el.src = "...";
+// el.src = "...";
 
-const productEl = document.querySelector(".product");
-console.log(productEl.getAttribute("data-lang"));
-console.log(productEl.getAttribute("class"));
-productEl.setAttribute("data-lang", "english");
+// const productEl = document.querySelector(".product");
+// console.log(productEl.getAttribute("data-lang"));
+// console.log(productEl.getAttribute("class"));
+// productEl.setAttribute("data-lang", "english");
 
-console.log(productEl.attributes);
+// console.log(productEl.attributes);
 
-console.log(productEl.dataset);
-productEl.dataset.hello = "Hi";
-productEl.dataset.asdad = "asdasd";
-delete productEl.dataset.hello;
+// console.log(productEl.dataset);
+// productEl.dataset.hello = "Hi";
+// productEl.dataset.asdad = "asdasd";
+// delete productEl.dataset.hello;
+
+// const pTag = document.querySelector("p");
+// console.log(pTag.style);
+// pTag.style.marginTop = "50px";
+
+// console.log(getComputedStyle(pTag));
 
