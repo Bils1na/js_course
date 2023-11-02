93dfd982a95b23f885f7895a508acf9b534d3edb
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-02 23:15:26 +0300

    fix

--- lesson5/main.js
+++ lesson5/main.js
@@ -8,3 +8,13 @@ el.src = "...";
 
 const productEl = document.querySelector(".product");
 console.log(productEl.getAttribute("data-lang"));
+console.log(productEl.getAttribute("class"));
+productEl.setAttribute("data-lang", "english");
+
+console.log(productEl.attributes);
+
+console.log(productEl.dataset);
+productEl.dataset.hello = "Hi";
+productEl.dataset.asdad = "asdasd";
+delete productEl.dataset.hello;
+
