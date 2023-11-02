9de32519f713a5bc6ec667c4da157bb42c691665
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-02 23:09:20 +0300

    added les5

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,2 +1,10 @@
 "use strict";
 
+const btnEl = document.querySelector("button");
+btnEl.id;
+btnEl.id = "asd";
+
+el.src = "...";
+
+const productEl = document.querySelector(".product");
+console.log(productEl.getAttribute("data-lang"));
