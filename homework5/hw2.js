ae1f6d73c96d08bac4e774046b388a00017fce2f
bilsina <bils1n@icloud.com>
2023-11-07 19:00:02 +0300

    Added task2half

--- homework5/hw2.js
+++ homework5/hw2.js
@@ -1,2 +1,38 @@
 "use strict";
 
+/*
+  1. Ко всем элементам, имеющим класс "dropdown-item" добавить еще один класс
+  "super-dropdown", необходимо использовать методы forEach и querySelectorAll и
+  свойство classList у элементов.
+   */
+
+const dropdownEls = document.querySelectorAll(".dropdown-item");
+dropdownEls.forEach(function(el) {
+    el.classList.add("super-dropdown");
+});
+
+console.log(dropdownEls);
+
+/*
+  2. У элемента с классом btn необходимо убрать класс "btn-secondary", если он
+  присутствует у этого элемента, либо добавить, если такого класса у элемента
+  не было.
+   */
+
+const btnEls = document.querySelector(".btn");
+btnEls.classList.toggle("btn-secondary");
+
+/*
+  3. Необходимо удалить класс "dropdown-menu" у элемента, у которого
+  присутствует класс "menu".
+   */
+const menuEl = document.querySelector(".menu").classList.remove("dropdown-menu");
+
+/*
+  4. Используя метод insertAdjacentHTML добавьте после div'a с классом
+  "dropdown" следующую разметку:
+    <a href="#">link</a>
+   */
+
+document.querySelector("div.dropdown")
+        .insertAdjacentHTML("afterend", "<a href=\"#\">link</a>");
