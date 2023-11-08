a17186451966cea86f3f85a9dffd8d646b615dee
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-08 20:42:40 +0300

    added les6 half 2

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,23 +1,45 @@
 "use strict";
 
-const aEl = document.querySelector("a");
-aEl.addEventListener("click", event => {
-    event.preventDefault();
-    console.log("Hello");
+// const aEl = document.querySelector("a");
+// aEl.addEventListener("click", event => {
+//     event.preventDefault();
+//     console.log("Hello");
+// });
+
+// document.addEventListener("contextmenu", event => {
+//     event.preventDefault();
+//     console.log(event.clientX, event.clientY);
+// });
+
+// document.addEventListener("copy", event => {
+//     event.preventDefault();
+//     const copiedText = document.getSelection().toString();
+//     // console.log(copiedText);
+//     event.clipboardData.setData("text/plain", `${copiedText} - this is copy`);
+// });
+
+// document.addEventListener("cut", event => {
+//     event.preventDefault();
+// });
+
+// document.addEventListener("dblclick", () => {
+//     console.log(123);
+// });
+
+// window.addEventListener("resize", event => {
+//     console.log(window.innerWidth, window.innerHeight);
+//     console.log(document.documentElement.clientWidth,
+//                 document.documentElement.clientHeight);
+// });
+
+document.querySelector("input").addEventListener("blur", () => {
+    console.log("blur");
 });
 
-document.addEventListener("contextmenu", event => {
-    event.preventDefault();
-    console.log(event.clientX, event.clientY);
+document.querySelector("input").addEventListener("change", () => {
+    console.log("change");
 });
 
-document.addEventListener("copy", event => {
-    event.preventDefault();
-    const copiedText = document.getSelection().toString();
-    // console.log(copiedText);
-    event.clipboardData.setData("text/plain", `${copiedText} - this is copy`);
-});
-
-document.addEventListener("cut", event => {
-    event.preventDefault();
+document.querySelector("input").addEventListener("input", () => {
+    console.log("input");
 });
