b5b0a244142ead841ea35bb8ccecbef3863b4aac
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-12-01 11:41:22 +0300

    added structure

--- lesson7/main.js
+++ lesson7/main.js
@@ -64,9 +64,49 @@
 
 // user.setName("Pavel").setSurname("Tarasov").setAge(25);
 
-const arr1 = [1, 5, 6];
-const arr2 = [100, ...arr1];
+// const arr1 = [1, 5, 6];
+// const arr2 = [100, ...arr1];
 
-// for (let i = 0; i < arr1.length; i++) {
-//     arr2.push(arr1[i]);
+// // for (let i = 0; i < arr1.length; i++) {
+// //     arr2.push(arr1[i]);
+// // }
+
+// const obj1 = { a: 5, b: 6,
+//     [Symbol.iterator]: function* () {
+//         yield 5;
+//         yield 20;
+//     }, 
+// };
+// const obj2 = { asd: 100, ...obj1};
+
+
+// for (const key in obj1) {
+//     obj2[key] = obj1[key];
 // }
+
+// function fn(...rest) {
+//     console.log(rest);
+// }
+
+// fn(...arr1);
+
+const arr1 = [1, 5, 6];
+// const a = arr1[0];
+// const b = arr1[1];
+
+const[a,, b] = arr1;
+console.log(a, b); // 1 6
+
+const obj1 = { a: 5, b: 6, c: 10 };
+const { a, c } = obj1;
+console.log(a, c) // 5 10
+
+let a = 5;
+let b = 10;
+[a, b] = [b, a];
+
+function fn({a, c: cParam = 100}) {
+    console.log(a, cParam);
+}
+
+fn(obj1); // 5 10
